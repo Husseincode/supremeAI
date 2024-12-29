62e77e7ed862bbd13b70e8f99a6ca166e52aac36
Akanji Abayomi Hussein <akanjiabayomi2@gmail.com>
2024-12-29 18:20:55 +0100

    committing new changes

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -7,29 +7,56 @@ import axios from 'axios';
 import LoadingBalls from '../components/loading/loading';
 import '../styles/styles.css';
 import Image from 'next/image';
+import { marked } from 'marked';
 
 /** Message Interface */
 interface Message {
-  sender: 'user' | 'bot';
+  role: 'user' | 'bot';
   content: string;
 }
 
 const ChatBot = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [input, setInput] = useState<string>('');
-  const messagesEndRef = useRef<HTMLDivElement>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [color, setColor] = useState<string>('#9ca3af');
+  const [sendButtonColor, setSendButtonColor] = useState<string>('#9ca3af');
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  //const [image, setImage] = useState<File | null>(null);
 
   /** Scroll to the latest message */
-  useEffect(() => {
+  const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  // Use marked to convert Markdown to HTML
+  const convertMarkdownToHtml = (content: string) => {
+    return marked(content); // Converts Markdown to HTML
+  };
+
+  // Handle file input change
+  // const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (e.target.files) {
+  //     setImage(e.target.files[0]);
+  //   }
+  // };
+
+  useEffect(() => {
+    scrollToBottom();
   }, [messages]);
 
   /** Load chat history on mount */
   useEffect(() => {
-    const history = getChatHistory();
-    setMessages(history);
+    const loadChatHistory = () => {
+      try {
+        const history = localStorage.getItem('chatHistory');
+        if (history) {
+          setMessages(JSON.parse(history));
+        }
+      } catch (error) {
+        console.error('Error loading chat history:', error);
+      }
+    };
+    loadChatHistory();
   }, []);
 
   /** Save chat history to localStorage */
@@ -37,39 +64,35 @@ const ChatBot = () => {
     localStorage.setItem('chatHistory', JSON.stringify(messages));
   };
 
-  /** Retrieve chat history from localStorage */
-  const getChatHistory = (): Message[] => {
-    try {
-      const history = localStorage.getItem('chatHistory');
-      return history ? JSON.parse(history) : [];
-    } catch (error) {
-      console.error('Error parsing chat history:', error);
-      return [];
-    }
-  };
-
   /** Send a message */
   const sendMessage = async () => {
-    if (input.trim() === '') return;
+    if (!input.trim()) return;
 
-    const userMessage: Message = { sender: 'user', content: input };
+    const userMessage: Message = { role: 'user', content: input };
     const updatedMessages = [...messages, userMessage];
     setMessages(updatedMessages);
     saveChatHistory(updatedMessages);
 
     setInput('');
+    setSendButtonColor('#9ca3af');
     setIsLoading(true);
 
     try {
-      const res = await axios.post('/api/chat', { message: input });
-      const botMessage: Message = { sender: 'bot', content: res.data.response };
+      const payload = { message: input };
+      const res = await axios.post('/api/chat', payload);
+
+      const botMessage: Message = {
+        role: 'bot',
+        content: res.data.response,
+      };
+
       const newMessages = [...updatedMessages, botMessage];
       setMessages(newMessages);
       saveChatHistory(newMessages);
     } catch (error) {
       console.error('Error sending message:', error);
       const errorMessage: Message = {
-        sender: 'bot',
+        role: 'bot',
         content: 'Sorry, something went wrong!',
       };
       const errorMessages = [...updatedMessages, errorMessage];
@@ -88,88 +111,114 @@ const ChatBot = () => {
     }
   };
 
-  return (
-    <section className='w-full flex md:justify-center md:items-center slide-from-left md:px-0'>
+  const renderMessageContent = (message: string) => {
+    // // Check if the message is code by looking for code block indicators (optional)
+    // const isCode = message.includes('```');
+    // if (isCode) {
+    //   // Remove the surrounding ``` for display
+    //   return (
+    //     <pre className=''>
+    //       <code>{message.replace(/```/g, '')}</code>
+    //     </pre>
+    //   );
+    // }
+    // return (
+    //   <div
+    //     className='message-content'
+    //     dangerouslySetInnerHTML={{ __html: message }} // This will safely inject the HTML/Markdown content
+    //   />
+    // );
+    // Convert the message content to HTML if it's Markdown
+    const htmlContent = convertMarkdownToHtml(message);
+
+    return (
       <div
-        ref={messagesEndRef}
-        className='w-full md:w-[350px] md:h-[500px] py-2 px-4 md:rounded-md shadow-xl bg-zinc-900 flex flex-col gap-2'>
-        <div className='header justify-center w-full h-[40px] rounded-md flex items-center px-2'>
-          <div className='flex items-center justify-center'>
-            {' '}
-            <h2 className='text-2xl text-white font-medium'>Jarvis</h2>
-            <Image
-              src={defaultUser}
-              width={30}
-              height={30}
-              className='rounded-full'
-              alt=''
-            />
-          </div>
-        </div>
-        <div
-          className={`chatWallPaper w-full border md:h-[300px] h-[500px] overflow-y-scroll scrollbar-hide rounded-md p-2`}>
-          <div className='flex-1 py-4 overflow-y-scroll scrollbar-hide'>
-            {messages.map((msg, idx) => (
-              <div
-                key={idx}
-                className={`flex gap-1 mb-4 flex-wrap ${
-                  msg.sender === 'user' ? 'justify-end' : 'justify-start'
-                }`}>
-                {msg.sender === 'bot' && (
-                  <div className='w-[25px] h-[25px] bg-white text-center pt-1 font-bold border rounded-full text-xs'>
-                    J
-                  </div>
-                )}
-                <div className='flex flex-wrap text-sm px-4 py-2 rounded-lg border bg-white text-gray-800'>
-                  {msg.content}
-                </div>
-              </div>
-            ))}
-            {isLoading && (
-              <div className='flex justify-start mb-4'>
-                <span className='bg-gray-200 text-sm text-gray-800 p-2 rounded-lg flex items-center gap-1'>
-                  <span>Jarvis is typing...</span> <LoadingBalls />
-                </span>
-              </div>
-            )}
-            <div ref={messagesEndRef} />
-          </div>
-        </div>
-        <div className='min-h-[90px] border rounded-md bg-zinc-500 flex items-center justify-between px-2'>
-          <textarea
-            value={input}
-            onKeyPress={handleKeyPress}
-            onChange={(e) => {
-              setInput(e.target.value);
-              setColor(input !== '' ? '#000000' : '#9ca3af');
-            }}
-            className='outline-none px-2 pt-4 pb-2 bg-transparent w-full text-sm resize-none'
-            placeholder='Type a message...'
+        className='message-content'
+        dangerouslySetInnerHTML={{ __html: htmlContent }} // This will safely inject the HTML/Markdown content
+      />
+    );
+  };
+
+  return (
+    <section className='w-full h-screen flex md:justify-center md:items-center slide-from-left md:px-0'>
+      <div className='w-full md:w-[350px] md:h-[500px] py-2 px-4 md:rounded-md shadow-xl bg-zinc-900 flex flex-col gap-2'>
+        <div className='header flex justify-center w-full h-[40px] rounded-md items-center px-2'>
+          <h2 className='text-2xl text-white font-medium'>Jarvis</h2>
+          <Image
+            src={defaultUser}
+            width={30}
+            height={30}
+            className='rounded-full ml-2'
+            alt='Default User'
           />
-          <button type='button' onClick={sendMessage}>
-            {''}
-            <svg
-              width='32'
-              height='32'
-              viewBox='0 0 32 32'
-              className='cursor-pointer'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'>
-              <rect width='32' height='32' rx='16' fill={color} />
-              <path
-                d='M9.72362 9.05279C9.55282 8.96739 9.34828 8.98673 9.19652 9.10265C9.04476 9.21857 8.97227 9.4108 9.00973 9.59806L10.4126 14.4478C10.466 14.6325 10.6207 14.7702 10.8103 14.802L16.5 15.7549C16.768 15.8084 16.768 16.1915 16.5 16.2451L10.8103 17.198C10.6207 17.2298 10.466 17.3675 10.4126 17.5522L9.00973 22.4019C8.97227 22.5892 9.04476 22.7814 9.19652 22.8974C9.34828 23.0133 9.55282 23.0326 9.72362 22.9472L22.7236 16.4472C22.893 16.3625 23 16.1894 23 16C23 15.8106 22.893 15.6375 22.7236 15.5528L9.72362 9.05279Z'
-                fill='#FCFCFC'
-              />
-            </svg>
-          </button>
+        </div>
+        <div className='flex flex-col gap-10'>
+          <div className='chatWallPaper w-full md:h-[300px] h-[500px] overflow-y-scroll scrollbar-hide rounded-md p-2'>
+            <div className='flex-1 py-4 overflow-y-scroll scrollbar-hide'>
+              {messages.map((msg, idx) => (
+                <div
+                  key={idx}
+                  className={`flex gap-1 mb-4 flex-wrap ${
+                    msg.role === 'user' ? 'justify-end' : 'justify-start'
+                  }`}>
+                  {msg.role === 'bot' && (
+                    <div className='w-[25px] h-[25px] bg-white text-center pt-1 font-bold border rounded-full text-xs'>
+                      J
+                    </div>
+                  )}
+                  <div
+                    className={`flex flex-wrap break-words overflow-scroll scrollbar-hide text-sm px-4 py-2 rounded-lg border ${
+                      msg.role === 'user' ? 'bg-gray-100' : 'bg-gray-300'
+                    } text-gray-800`}>
+                    {renderMessageContent(msg.content)}
+                  </div>
+                </div>
+              ))}
+              {isLoading && (
+                <div className='flex justify-start mb-4'>
+                  <span className='bg-gray-200 text-sm text-gray-800 p-2 rounded-lg flex items-center gap-1'>
+                    <span>Jarvis is typing...</span> <LoadingBalls />
+                  </span>
+                </div>
+              )}
+              <div ref={messagesEndRef} />
+            </div>
+          </div>
+
+          <div className='min-h-[90px] border rounded-md bg-zinc-500 flex items-center justify-between px-2'>
+            <textarea
+              value={input}
+              onKeyPress={handleKeyPress}
+              onChange={(e) => {
+                setInput(e.target.value);
+                setSendButtonColor(
+                  e.target.value.trim() ? '#000000' : '#9ca3af'
+                );
+              }}
+              className='outline-none px-2 pt-4 pb-2 bg-transparent w-full text-sm resize-none'
+              placeholder='Type a message...'
+            />
+            <button type='button' onClick={sendMessage}>
+              {''}
+              <svg
+                width='32'
+                height='32'
+                viewBox='0 0 32 32'
+                className='cursor-pointer'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'>
+                <rect width='32' height='32' rx='16' fill={sendButtonColor} />
+                <path
+                  d='M9.72362 9.05279C9.55282 8.96739 9.34828 8.98673 9.19652 9.10265C9.04476 9.21857 8.97227 9.4108 9.00973 9.59806L10.4126 14.4478C10.466 14.6325 10.6207 14.7702 10.8103 14.802L16.5 15.7549C16.768 15.8084 16.768 16.1915 16.5 16.2451L10.8103 17.198C10.6207 17.2298 10.466 17.3675 10.4126 17.5522L9.00973 22.4019C8.97227 22.5892 9.04476 22.7814 9.19652 22.8974C9.34828 23.0133 9.55282 23.0326 9.72362 22.9472L22.7236 16.4472C22.893 16.3625 23 16.1894 23 16C23 15.8106 22.893 15.6375 22.7236 15.5528L9.72362 9.05279Z'
+                  fill='#FCFCFC'
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
     </section>
   );
 };
-
-// const chatWallpaper = {
-//   background: `url(${})`
-// }
 
 export default ChatBot;
--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -22,6 +22,17 @@ export default async function handler(
     return res.status(400).json({ error: 'Message is required' });
   }
 
+  // if (!process.env.OPENAI_API_KEY) {
+  //   console.error('Missing OpenAI API Key');
+  //   return res
+  //     .status(500)
+  //     .json({ error: 'Internal server configuration error' });
+  // }
+
+  // if (!Array.isArray(history)) {
+  //   return res.status(400).json({ error: 'Invalid history format' });
+  // }
+
   const jarvisAcknowledgment = (userMessage: string) => {
     if (userMessage.toLowerCase().includes('who created you')) {
       return "I was created by Akanji Abayomi, a skilled web developer based in Nigeria. I'm here to assist you with anything you need!";
@@ -44,12 +55,13 @@ export default async function handler(
           content:
             'You are Jarvis, an AI assistant created by Akanji Abayomi. You are friendly, intelligent, and eager to assist users with any questions or tasks.',
         },
+        // ...(Array.isArray(history) ? history : []),
         { role: 'user', content: message },
       ],
     });
 
     const content = response.choices[0]?.message?.content || 'No response';
-    console.log(response);
+    //console.log(response);
 
     return res.status(200).json({ response: content });
   } catch (error) {
